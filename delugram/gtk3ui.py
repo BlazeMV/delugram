a3d0ac0294d217e79609fa8926d0562ec02cda8f
BlazeMv <ad.adamdavid72@gmail.com>
2025-02-15 18:13:56 +0500

    gtk3ui init

--- delugram/gtk3ui.py
+++ delugram/gtk3ui.py
@@ -1,15 +1,3 @@
-# -*- coding: utf-8 -*-
-# Copyright (C) 2025 Your Name <yourname@example.com>
-#
-# Basic plugin template created by the Deluge Team.
-#
-# This file is part of MyPlugin2 and is licensed under GNU GPL 3.0, or later,
-# with the additional special exception to link portions of this program with
-# the OpenSSL library. See LICENSE for more details.
-from __future__ import unicode_literals
-
-import logging
-
 from gi.repository import Gtk
 
 import deluge.component as component
@@ -17,9 +5,7 @@ from deluge.plugins.pluginbase import Gtk3PluginBase
 from deluge.ui.client import client
 
 from .common import get_resource
-
-log = logging.getLogger(__name__)
-
+from delugram.logger import log
 
 class Gtk3UI(Gtk3PluginBase):
     def enable(self):
@@ -27,29 +13,29 @@ class Gtk3UI(Gtk3PluginBase):
         self.builder.add_from_file(get_resource('config.ui'))
 
         component.get('Preferences').add_page(
-            'MyPlugin2', self.builder.get_object('prefs_box'))
+            'Delugram', self.builder.get_object('prefs_box'))
         component.get('PluginManager').register_hook(
             'on_apply_prefs', self.on_apply_prefs)
         component.get('PluginManager').register_hook(
             'on_show_prefs', self.on_show_prefs)
 
     def disable(self):
-        component.get('Preferences').remove_page('MyPlugin2')
+        component.get('Preferences').remove_page('Delugram')
         component.get('PluginManager').deregister_hook(
             'on_apply_prefs', self.on_apply_prefs)
         component.get('PluginManager').deregister_hook(
             'on_show_prefs', self.on_show_prefs)
 
     def on_apply_prefs(self):
-        log.debug('applying prefs for MyPlugin2')
+        log.debug('applying prefs for Delugram')
         config = {
-            'test': self.builder.get_object('txt_test').get_text()
+            'telegram_token': self.builder.get_object('txt_test').get_text()
         }
-        client.myplugin2.set_config(config)
+        # client.delugram.set_config(config)
 
     def on_show_prefs(self):
-        client.myplugin2.get_config().addCallback(self.cb_get_config)
+        client.delugram.get_config().addCallback(self.cb_get_config)
 
     def cb_get_config(self, config):
         """callback for on show_prefs"""
-        self.builder.get_object('txt_test').set_text(config['test'])
+        # self.builder.get_object('txt_test').set_text(config['telegram_token'])
