9b933209f30b7aa7641409b7a9787f239f17edff
BlazeMv <ad.adamdavid72@gmail.com>
2025-02-18 21:26:04 +0500

    Suppress logs from httpx

--- delugram/logger.py
+++ delugram/logger.py
@@ -6,7 +6,12 @@ logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=LOG_LEVEL
 )
 
+
+
 log = logging.getLogger("delugram")
 
 # Suppress logs from deluge.core.alertmanager
 logging.getLogger("deluge.core.alertmanager").setLevel(logging.INFO)
+
+# Suppress logs from httpx
+logging.getLogger("httpx").setLevel(logging.WARNING)
